73b250315616bf5cd0065c2793bee3c5eb958ea3
YovkovIT <yovkovyoan88@gmail.com>
2024-04-14 12:29:45 +0300

    to be continued

---
+++ Exercise Multidimensional Arrays/Diagonal Difference/Program.cs
@@ -0,0 +1,25 @@
+﻿int matrixSize = int.Parse(Console.ReadLine());
+int rows = matrixSize;
+int cols = matrixSize;
+int[,] matrix = new int[rows, cols]; 
+for (int i = 0; i < rows; i++)
+{
+    int[] tokens = Console.ReadLine()
+        .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+    for (int j = 0; j < cols; j++)
+    {
+        matrix[i,j] = tokens[j];
+    }
+}
+
+int firstPrimary = 0;
+int secondPrimary = 0;
+
+for (int i = 0; i < rows; i++)
+{
+    firstPrimary += matrix[i, i];
+    secondPrimary += matrix[rows - 1 - i, i];
+}
+Console.WriteLine($"{Math.Abs(firstPrimary - secondPrimary)}");
---
+++ Exercise Multidimensional Arrays/Squares in Matrix/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] matrixSize = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+int rows = matrixSize[0];
+int cols = matrixSize[1];
+
+int[,] matrix = new int[rows, cols]; 
+int sumEqual = 0;
+for (int row = 0; row < rows; row++)
+{
+    char[] tokens = Console.ReadLine()
+        .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+        .Select(char.Parse)
+        .ToArray();
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row,col] = tokens[col];
+    }
+}
+
+for (int row = 0;row < rows - 1; row++)
+{
+    for (int col = 0;col < cols - 1; col++)
+    {
+        if (matrix[row,col] == matrix[row + 1, col] && matrix[row,col] == matrix[row,col+1] && matrix[row,col] == matrix[row + 1, col + 1])
+        {
+            sumEqual++;
+        }
+    }
+}
+Console.WriteLine(sumEqual);
