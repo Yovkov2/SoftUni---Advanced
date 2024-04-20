d4fda65d72a66bd1abfd85426b21f23f66d2028e
YovkovIT <yovkovyoan88@gmail.com>
2024-04-20 23:22:18 +0300

    basic class

---
+++ Lab Defining Classes/Car.cs
@@ -0,0 +1,69 @@
+﻿namespace CarManufacturer
+{
+    public class Car 
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public void Drive(double distance)
+        {
+            double fuelNeeded = distance * FuelConsumption;
+            if (FuelQuantity >= fuelNeeded)
+            {
+                FuelQuantity -= fuelNeeded;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public virtual string WhoAmI()
+        {
+            return $"Make: {this.Make} Model: {this.Model} Year: {this.Year} Fuel: {this.FuelQuantity:F2}";
+        }
+
+    }
+
+}
---
+++ Lab Defining Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+            Car car = new Car(make, model, year , fuelQuantity, fuelConsumption);
+            car.Drive(2000);
+
+            Console.WriteLine(car.WhoAmI());
+
+        }
+    }
+
+}
