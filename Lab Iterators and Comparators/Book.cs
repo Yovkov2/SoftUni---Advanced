6b9b77730585cb441f1f5d035d63e78a4595b2b4
YovkovIT <yovkovyoan88@gmail.com>
2024-05-08 15:16:15 +0300

    iterators

---
+++ Lab Iterators and Comparators/Book.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyCollection<string> Authors { get; set; }
+        public int CompareTo(Book other)
+        {
+            int result = this.Year.CompareTo(other.Year);
+            if(result == 0)
+            {
+                result = this.Title.CompareTo(other.Title);
+            }
+            return result;
+        }
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
---
+++ Lab Iterators and Comparators/Library.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
